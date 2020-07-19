aa6d6b7007dd755d2c382a2e4dc9ebe882d05540
Rodolfo Marriel <rodolfo.29r@gmail.com>
2020-07-19 20:18:59 -0300

    begining home and about

---
+++ src/components/Profile/index.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Profile = () => (
+  <div className="Profile-wrapper">
+    <h1>John Doe</h1>
+    <h2>Programador Fullstack</h2>
+    <p>lorem ipsum in dolor</p>
+  </div>
+)
+
+export default Profile
